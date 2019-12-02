8b2c41372021d72affb4b492b08e71a916a8e975
JoaoRMaia <48926555+JoaoRMaia@users.noreply.github.com>
2019-12-01 23:46:27 -0300

    +Pagina

--- ServBD/node-mysql-bd/routes/index.js
+++ ServBD/node-mysql-bd/routes/index.js
@@ -7,17 +7,32 @@ module.exports = {
                 res.redirect('/');
             }
 
-        let query = "SELECT Fornecedor, SUM(Reembolso) FROM CEAPS INNER JOIN Senador ON Sen_ID=Senador.ID GROUP BY Fornecedor ;"
+        let query = "SELECT Fornecedor, SUM(Reembolso) as 'a' FROM CEAPS INNER JOIN Senador ON Sen_ID=Senador.ID GROUP BY Fornecedor LIMIT 10;"
                 db.query(query, (err, result2) => {
             if (err) {
                 res.redirect('/');
             }
-            
+        let query = "SELECT Partido, Sum(Valor) as 'a' FROM Senador INNER JOIN Recebe ON Recebe.fk_Sen_ID = Senador.ID INNER JOIN Beneficio ON Recebe.fk_Beneficio_Id = Beneficio.ID Group By Partido;"
+            db.query(query, (err, result3) => {
+            if (err) {
+            res.redirect('/');
+        }
+        let query = "select mes, sum(reembolso) as 'a' from ceaps where ano = '2019' group by mes ;"
+        db.query(query, (err, result4) => {
+        if (err) {
+        res.redirect('/');
+    }
+        
+                
              res.render("index.ejs", {
                 title: "Trabalho Banco de Dados"
                 ,senadores: result
-                ,fornecedor: result2
+                ,fornecedores: result2
+                ,partidos: result3
+                ,ceaps: result4
             });
+        });
+    });
         });
         });
     },
